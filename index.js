e6ec6c8db81b4011ff4872defd39bec144a25898
Peter Naydenov <free@backformation.com>
2017-11-26 04:39:54 +0200

    Refactoring Index
    - Separate help functions;
    - Lib for Templates related functions;
    - Lib for Process related functions;
    - Lib for Data related functions;

--- index.js
+++ index.js
@@ -39,268 +39,45 @@ function codeAssembly ( cfg ) {
 
 
 
-const lib = {
-  insertTemplate : function ( extTemplate ) { // (extTemplate: ExternalTemplate) -> void
-        let 
-            me = this
-          , templateNames = Object.keys( extTemplate )
-          ;
-
-        templateNames.forEach ( name => {
-              if ( lib._isTemplateWritingForbidden(me,name) ) {
-                    console.error ( showError('overwriteTemplate') )
-                    return
-                  }
-              me.templates [ name ] = interpretTemplate ( extTemplate[name] )
-            })        
-  } // insertTemplate func.
 
 
 
 
 
-, insertTemplateLib : function ( extLib, libName ) {  //   ( extLib: ExternalTemplate,  name: string ) -> void
-      let 
-              me = this
-          ,  simpleTemplates = Object.keys ( extLib )
-          ;
-
-      simpleTemplates.forEach ( extName => {
-                                const 
-                                        newTpl = {}
-                                      , name = `${libName}/${extName}`
-                                      ;
-                                if ( lib._isTemplateWritingForbidden(me,name) ) {
-                                        console.error ( showError('overwriteTemplate')   )
-                                        return
-                                   }
-                                newTpl[name] = extLib[extName]
-                                lib.insertTemplate.call ( me, newTpl )
-                        })
-  } // insertTemplateLib func.
 
 
 
 
 
-, _isTemplateWritingForbidden : function ( me , name ) {
+
+
+
+
+
+
+const help = {
+
+  _isTemplateWritingForbidden ( engine , name ) {
     const
-           entryExists = me.templates.hasOwnProperty ( name ) // 'true' if template with this name already exists
-         , entryForbidden = entryExists && !me.config.overwriteTemplates
+           entryExists = engine.templates.hasOwnProperty ( name ) // 'true' if template with this name already exists
+         , entryForbidden = entryExists && !engine.config.overwriteTemplates
          ;
    return entryForbidden
 } // _isTemplateWritingForbidden func.
 
 
 
-
-
-, renameTemplate : function ( ops ) { //   ({oldName:newName}) -> void
-  // * Change template names
-  const 
-            me = this
-          , list = Object.keys (ops)
-          , doOverwrite = me.config.overwriteTemplates
-          ;
-  
-  list.forEach ( key => {
-          const 
-                  newKey = ops[key]
-                , keyExists = (me.templates[newKey]) ? true : false
-                ;
-
-          if ( keyExists && !doOverwrite ){
-                  console.error ( showError('overwriteTemplate') )
-                  return
-               }
-          else {
-                  me.templates[newKey] = me.templates[key]
-                  delete me.templates[key]
-            }
-    })
-} // renameTemplate func.
-
-
-
-
-
-, removeTemplate : function ( tplName ) {   //   ( tplName:string|string[]) -> void
-    const me = this;
-    let listDelete;
-
-    if ( tplName instanceof Array ) listDelete = tplName
-    else {
-            const t = Object.keys ( arguments );
-            listDelete = t.map ( k => arguments[k] )
-         }
-    
-    listDelete.forEach ( item => delete me.templates[item])
-} // removeTemplate func.
-
-
-
-
-
-, getTemplate: function ( tplName ) { // (tplName: string|string[]) -> ExternalTemplate
-  const 
-        me = this
-      , tpl = me.templates
-      ;
-  let tplRequst;
-
-  if ( tplName instanceof Array )   tplRequst = tplName
-  else {
-          const t = Object.keys ( arguments );
-          tplRequst = t.map ( k => arguments[k] )
-       }
-  
-  return tplRequst.reduce ( (res, item ) => {
-                      if ( tpl[item] ) res[item] = tpl[item].tpl.join('')
-                      else             res[item] = ''
-                      return res
-                },{})
-
-} // getTemplate func.
-
-
-
-
-
-, getTemplateLib: function ( libName ) { //   (libName:string|string[]) -> ExternalTemplate
-  const me = this;
-  let libRequst;
-
-  if ( libName instanceof Array )   libRequst = libName
-  else {
-          const t = Object.keys ( arguments );
-          libRequst = t.map ( k => arguments[k] )
-       }
-  
-  return lib._extractLib ( me.templates, libRequst )
-} // getTemplateLib func.
-
-
-
-
-
-, _extractLib ( tpl, libRequst ) { //   (tpl:inTemplates[], libRequest:string[]) -> ExternalTemplate
-  // * Creates new ExternalTemplate by removing libName from template name 
-  const tplNames = Object.keys ( tpl ).filter ( name => name.includes('/')   );
-
-  return libRequst.reduce ( (res, libItem ) => {
-                tplNames
-                    .filter  ( name => name.indexOf(libItem) == 0   )
-                    .forEach ( name => {
-                              const 
-                                    sliceIndex = name.indexOf('/')
-                                  , prop = name.slice ( sliceIndex+1 )
-                                  ;
-                              
-                              res [prop] = tpl[name].tpl.join('')
-                        })
-                return res
-            },{})
-} // _extractLib func.
-
-
-
-
-
-, _isProcessWritingForbidden : function ( me , name ) {
+, _isProcessWritingForbidden ( engine , name ) {
   const
-         entryExists = me.processes.hasOwnProperty ( name ) // 'true' if process with this name already exists
-       , entryForbidden = entryExists && !me.config.overwriteProcesses
+         entryExists = engine.processes.hasOwnProperty ( name ) // 'true' if process with this name already exists
+       , entryForbidden = entryExists && !engine.config.overwriteProcesses
        ;
  return entryForbidden
 } // _isProcessWritingForbidden func.
 
 
 
-
-
-, insertProcess: function ( ext, name ) { // (ext: extProcess, name: string) -> void
-    const me = this;
-
-    if ( lib._isProcessWritingForbidden(me,name) ) {
-          console.error ( showError('overwriteProcess',name) )
-          return
-        }
-
-    me.processes[name] = processTools.interpret ( ext )
-} // insertProcess func.
-
-
-
-
-
-, mixProcess : function ( mixList, newProcessName ) {   //   ( mixList:string[], processName:string ) -> void
-  // * Set new process as combination of existing processes
-    const
-            me            = this
-          , processes     = me.processes
-          ;
-          
-    let mix = {};   // new process container
-    
-    if ( lib._isProcessWritingForbidden(me,newProcessName) ) {
-          console.error ( showError('overwriteProcess', newProcessName)  )
-          return
-       }
-
-    mix.steps = []
-    mix.arguments =[]
-    mix.hooks = []
-    
-    mixList.forEach ( requestedName => {
-              if ( !processes[requestedName] ) return
-              const 
-                       source = processes[requestedName]
-                     , hookNames = source.hooks.map ( name => `${requestedName}/${name}` )
-                     , newArguments = source.arguments.map ( arg => { 
-                                                if ( arg.do != 'hook' ) return arg
-                                                return { do: 'hook', name: `${requestedName}/${arg.name}` }
-                                            })
-                     ;
-                     
-              mix['steps'] = mix['steps'].concat ( source['steps'] )
-              mix['arguments'] = mix['arguments'].concat ( newArguments )
-              mix['hooks'] = mix['hooks'].concat ( hookNames )
-           })
-    me.processes[newProcessName] = mix
-} // mixProcess func.
-
-
-
-
-
-, getHooks   : function ( processName ) { //   (processName) -> { hookName : undefined }
-    const recordExists = this.processes.hasOwnProperty ( processName );
-    if ( recordExists )  { 
-              const hooks = this.processes[ processName ].hooks;  
-              return hooks.reduce ( (res, name) => {
-                              res[name] = undefined
-                              return res
-                        },{})
-        }
-    else      return {}
-} // getHooks func.
-
-
-
-
-
-, getPlaceholders : function ( templateName ) { //   (templateName:string) -> placeholderNames:string[].
-    const tpl = this.templates [ templateName ];
-
-    if ( tpl )  return Object.keys ( tpl.placeholders )
-    else        return []
-} // getPlaceholders func.
-
-
-
-
-
-, _validateProcess : function ( engine, processName ) {
+, _validateProcess ( engine, processName ) {
   // * Find if process exists and no errors in it. Find if all templates needed are available.
   let errors = [];
   const 
@@ -327,28 +104,314 @@ const lib = {
 } // _validateProcess func.
 
 
+, _extractLib ( tpl, libRequst ) { //   (tpl:inTemplates[], libRequest:string[]) -> ExternalTemplate
+  // * Creates new ExternalTemplate by removing libName from template name 
+  const tplNames = Object.keys ( tpl ).filter ( name => name.includes('/')   );
+
+  return libRequst.reduce ( (res, libItem ) => {
+                tplNames
+                    .filter  ( name => name.indexOf(libItem) == 0   )
+                    .forEach ( name => {
+                              const 
+                                    sliceIndex = name.indexOf('/')
+                                  , prop = name.slice ( sliceIndex+1 )
+                                  ;
+                              
+                              res [prop] = tpl[name].tpl.join('')
+                        })
+                return res
+            },{})
+} // _extractLib func.
+
+} // help lib
 
 
 
-, runProcess : function ( processList, data, hooks ) {    
-    if ( !(data        instanceof Array ))   data        = [data]
-    if ( !(processList instanceof Array ))   processList = [processList]
-    const error = processList.reduce ( (res, processName ) => {
-                                    const errorUpdate = lib._validateProcess(this, processName);
-                                    return res.concat ( errorUpdate )
-                        },[])
 
-    if ( error.length == 0 )  {
-          let current = data;
-          processList.forEach ( processName => {
-              current = processTools.run.call ( this, this.processes[processName], current, hooks )
-          })
-          return current
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const lib_Template = {
+
+  insert ( extTemplate ) {   // (extTemplate: ExternalTemplate) -> void
+    let 
+        me = this
+      , templateNames = Object.keys( extTemplate )
+      ;
+
+    templateNames.forEach ( name => {
+          if ( help._isTemplateWritingForbidden(me,name) ) {
+                console.error ( showError('overwriteTemplate') )
+                return
+              }
+          me.templates [ name ] = interpretTemplate ( extTemplate[name] )
+        })        
+} //   insert func.   -- Template
+
+
+
+, insertLib ( extLib, libName ) {  //   ( extLib: ExternalTemplate,  name: string ) -> void
+  let 
+          me = this
+      ,  simpleTemplates = Object.keys ( extLib )
+      ;
+
+  simpleTemplates.forEach ( extName => {
+                            const 
+                                    newTpl = {}
+                                  , name = `${libName}/${extName}`
+                                  ;
+                            if ( help._isTemplateWritingForbidden(me,name) ) {
+                                    console.error ( showError('overwriteTemplate')   )
+                                    return
+                               }
+                            newTpl[name] = extLib[extName]
+                            lib_Template.insert.call ( me, newTpl )
+                    })
+} //   insertLib func.  -- Template
+
+
+
+, rename ( ops ) { //   ({oldName:newName}) -> void
+// * Change template names
+const 
+          me = this
+        , list = Object.keys (ops)
+        , doOverwrite = me.config.overwriteTemplates
+        ;
+
+list.forEach ( key => {
+        const 
+                newKey = ops[key]
+              , keyExists = (me.templates[newKey]) ? true : false
+              ;
+
+        if ( keyExists && !doOverwrite ){
+                console.error ( showError('overwriteTemplate') )
+                return
+             }
+        else {
+                me.templates[newKey] = me.templates[key]
+                delete me.templates[key]
+          }
+  })
+} //   rename func.   -- Template
+
+
+
+, remove ( tplName ) {   //   ( tplName:string|string[]) -> void
+  const me = this;
+  let listDelete;
+
+  if ( tplName instanceof Array ) listDelete = tplName
+  else {
+          const t = Object.keys ( arguments );
+          listDelete = t.map ( k => arguments[k] )
+       }
+  
+  listDelete.forEach ( item => delete me.templates[item])
+} //   remove func.   -- Template
+
+
+
+, get ( tplName ) { // (tplName: string|string[]) -> ExternalTemplate
+ const 
+       me = this
+     , tpl = me.templates
+     ;
+ let tplRequst;
+
+ if ( tplName instanceof Array )   tplRequst = tplName
+ else {
+         const t = Object.keys ( arguments );
+         tplRequst = t.map ( k => arguments[k] )
       }
-    else  return error
-} // run func.
+ 
+ return tplRequst.reduce ( (res, item ) => {
+                     if ( tpl[item] ) res[item] = tpl[item].tpl.join('')
+                     else             res[item] = ''
+                     return res
+               },{})
 
-} // lib
+} //   get func.   -- Template
+
+
+
+, getLib ( libName ) { //   (libName:string|string[]) -> ExternalTemplate
+ const me = this;
+ let libRequst;
+
+ if ( libName instanceof Array )   libRequst = libName
+ else {
+         const t = Object.keys ( arguments );
+         libRequst = t.map ( k => arguments[k] )
+      }
+ 
+ return help._extractLib ( me.templates, libRequst )
+} // getLib func.   -- Template
+
+
+
+, getPlaceholders ( templateName ) { //   (templateName:string) -> placeholderNames:string[].
+  const tpl = this.templates [ templateName ];
+
+  if ( tpl )  return Object.keys ( tpl.placeholders )
+  else        return []
+} // getPlaceholders func.   -- Template
+
+} //   lib_Template lib
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const lib_Process = {
+
+  insert ( ext, name ) { // (ext: extProcess, name: string) -> void
+    const me = this;
+
+    if ( help._isProcessWritingForbidden(me,name) ) {
+          console.error ( showError('overwriteProcess',name) )
+          return
+        }
+
+    me.processes[name] = processTools.interpret ( ext )
+} //   insert func.   -- Process
+
+
+
+, mix ( mixList, newProcessName ) {   //   ( mixList:string[], processName:string ) -> void
+  // * Set new process as combination of existing processes
+    const
+            me            = this
+          , processes     = me.processes
+          ;
+          
+    let mix = {};   // new process container
+    
+    if ( help._isProcessWritingForbidden(me,newProcessName) ) {
+          console.error ( showError('overwriteProcess', newProcessName)  )
+          return
+       }
+
+    mix.steps = []
+    mix.arguments =[]
+    mix.hooks = []
+    
+    mixList.forEach ( requestedName => {
+              if ( !processes[requestedName] ) return
+              const 
+                       source = processes[requestedName]
+                     , hookNames = source.hooks.map ( name => `${requestedName}/${name}` )
+                     , newArguments = source.arguments.map ( arg => { 
+                                                if ( arg.do != 'hook' ) return arg
+                                                return { do: 'hook', name: `${requestedName}/${arg.name}` }
+                                            })
+                     ;
+                     
+              mix['steps'] = mix['steps'].concat ( source['steps'] )
+              mix['arguments'] = mix['arguments'].concat ( newArguments )
+              mix['hooks'] = mix['hooks'].concat ( hookNames )
+           })
+    me.processes[newProcessName] = mix
+} //   mix func.   -- Process
+
+
+
+, getHooks ( processName ) { //   (processName) -> { hookName : undefined }
+const recordExists = this.processes.hasOwnProperty ( processName );
+if ( recordExists )  { 
+          const hooks = this.processes[ processName ].hooks;  
+          return hooks.reduce ( (res, name) => {
+                          res[name] = undefined
+                          return res
+                    },{})
+    }
+else      return {}
+} //   getHooks func.   -- Process
+
+
+
+, run ( processList, data, hooks ) {    
+  if ( !(data        instanceof Array ))   data        = [data]
+  if ( !(processList instanceof Array ))   processList = [processList]
+  const error = processList.reduce ( (res, processName ) => {
+                                  const errorUpdate = help._validateProcess(this, processName);
+                                  return res.concat ( errorUpdate )
+                      },[])
+
+  if ( error.length == 0 )  {
+        let current = data;
+        processList.forEach ( processName => {
+            current = processTools.run.call ( this, this.processes[processName], current, hooks )
+        })
+        return current
+    }
+  else  return error
+} //   run func.   -- Process
+
+}   // lib_Process lib
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const lib_Data = {
+
+}   // lib_Data lib
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -364,24 +427,30 @@ codeAssembly.prototype = {
       tools : tools              // Usefull template and process related functions (external)
    
     // Template I/O Operations
-    , insertTemplate    : lib.insertTemplate     // Insert template;
-    , insertTemplateLib : lib.insertTemplateLib  // Insert template library;    
-    , getTemplate       : lib.getTemplate        // Exports template
-    , getTemplateLib    : lib.getTemplateLib     // Export list of templates as library
-    , getPlaceholders   : lib.getPlaceholders    // Return placeholders per template
+    , insertTemplate    : lib_Template.insert     // Insert templates;
+    , insertTemplateLib : lib_Template.insertLib  // Insert templates as a template-library;    
+    , getTemplate       : lib_Template.get        // Export templates;
+    , getTemplateLib    : lib_Template.getLib     // Export templates from template-library;
+    , getPlaceholders   : lib_Template.getPlaceholders    // Return placeholders per template;
       
     // Template Manipulation
-    , renameTemplate : lib.renameTemplate // change name of template/templates
-    , removeTemplate : lib.removeTemplate // remove template/templates
+    , renameTemplate : lib_Template.rename // change name of template/templates
+    , removeTemplate : lib_Template.remove // remove template/templates
 
     // Processes
-    , insertProcess : lib.insertProcess // Insert new process
-    , mixProcess    : lib.mixProcess    // Set new process as combination of existing processes
-    , getHooks      : lib.getHooks      // Provide information about hooks available
-    , run           : lib.runProcess    // Execute process
+    , insertProcess    : lib_Process.insert // Insert new process
+    , mixProcess       : lib_Process.mix    // Set new process as combination of existing processes
+    , insertProcessLib : 'NA'
+    , getProcess       : 'NA'
+    , getProcessLib    : 'NA'    
+    , getHooks         : lib_Process.getHooks      // Provide information about hooks available
+    , run              : lib_Process.run    // Execute process
 
-    // Data
-    , insertData : 'NA'   // Save data. Word 'blocks'
+    // Data I/O
+    , insertData    : 'NA'   // Insert data. Save data. Word 'blocks'
+    , insertDataLib : 'NA'   // Insert set of data
+    , getData       : 'NA'   // Get data
+    , getDataLib    : 'NA'   // Get list of data-records by removing 
 } // codeAssembly prototype
 
 
